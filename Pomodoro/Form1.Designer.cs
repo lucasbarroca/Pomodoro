c62991cdb49bd25f09ebcdfd7bf12659690cf6ee
Lucas Barroca <lucas.lbarroca@gmail.com>
2021-05-26 20:13:14 -0300

    First commit

---
+++ Pomodoro/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Pomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTime = new System.Windows.Forms.Label();
+            this.pgTime = new System.Windows.Forms.ProgressBar();
+            this.btPlayPause = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.timeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startStopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.breakToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.alwaysVisibleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.workToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.breakToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrSec = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTime.Location = new System.Drawing.Point(12, 33);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(61, 22);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pgTime
+            // 
+            this.pgTime.Location = new System.Drawing.Point(79, 35);
+            this.pgTime.Maximum = 1;
+            this.pgTime.Name = "pgTime";
+            this.pgTime.Size = new System.Drawing.Size(211, 22);
+            this.pgTime.Step = 1;
+            this.pgTime.TabIndex = 2;
+            // 
+            // btPlayPause
+            // 
+            this.btPlayPause.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btPlayPause.BackgroundImage = global::Pomodoro.Properties.Resources.play;
+            this.btPlayPause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btPlayPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btPlayPause.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btPlayPause.Location = new System.Drawing.Point(296, 35);
+            this.btPlayPause.Name = "btPlayPause";
+            this.btPlayPause.Size = new System.Drawing.Size(22, 22);
+            this.btPlayPause.TabIndex = 4;
+            this.btPlayPause.UseVisualStyleBackColor = false;
+            this.btPlayPause.Click += new System.EventHandler(this.btPlayPause_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timeToolStripMenuItem,
+            this.timeToolStripMenuItem1,
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(145, 1);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 2, 6, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(181, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // timeToolStripMenuItem
+            // 
+            this.timeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startStopToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.resetToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.alwaysVisibleToolStripMenuItem});
+            this.timeToolStripMenuItem.Name = "timeToolStripMenuItem";
+            this.timeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.timeToolStripMenuItem.Text = "Timer";
+            // 
+            // startStopToolStripMenuItem
+            // 
+            this.startStopToolStripMenuItem.Name = "startStopToolStripMenuItem";
+            this.startStopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.startStopToolStripMenuItem.Text = "Start / Pause";
+            this.startStopToolStripMenuItem.Click += new System.EventHandler(this.startStopToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.workToolStripMenuItem1,
+            this.toolStripSeparator2,
+            this.breakToolStripMenuItem1});
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            // 
+            // workToolStripMenuItem1
+            // 
+            this.workToolStripMenuItem1.Name = "workToolStripMenuItem1";
+            this.workToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.workToolStripMenuItem1.Text = "Work";
+            this.workToolStripMenuItem1.Click += new System.EventHandler(this.workToolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(100, 6);
+            // 
+            // breakToolStripMenuItem1
+            // 
+            this.breakToolStripMenuItem1.Name = "breakToolStripMenuItem1";
+            this.breakToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.breakToolStripMenuItem1.Text = "Break";
+            this.breakToolStripMenuItem1.Click += new System.EventHandler(this.breakToolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(177, 6);
+            // 
+            // alwaysVisibleToolStripMenuItem
+            // 
+            this.alwaysVisibleToolStripMenuItem.Checked = true;
+            this.alwaysVisibleToolStripMenuItem.CheckOnClick = true;
+            this.alwaysVisibleToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.alwaysVisibleToolStripMenuItem.Name = "alwaysVisibleToolStripMenuItem";
+            this.alwaysVisibleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.alwaysVisibleToolStripMenuItem.Text = "Always Visible";
+            // 
+            // timeToolStripMenuItem1
+            // 
+            this.timeToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.workToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.breakToolStripMenuItem});
+            this.timeToolStripMenuItem1.Name = "timeToolStripMenuItem1";
+            this.timeToolStripMenuItem1.Size = new System.Drawing.Size(69, 20);
+            this.timeToolStripMenuItem1.Text = "Edit Time";
+            // 
+            // workToolStripMenuItem
+            // 
+            this.workToolStripMenuItem.Name = "workToolStripMenuItem";
+            this.workToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.workToolStripMenuItem.Text = "Work";
+            this.workToolStripMenuItem.Click += new System.EventHandler(this.workToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // breakToolStripMenuItem
+            // 
+            this.breakToolStripMenuItem.Name = "breakToolStripMenuItem";
+            this.breakToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.breakToolStripMenuItem.Text = "Break";
+            this.breakToolStripMenuItem.Click += new System.EventHandler(this.breakToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // tmrSec
+            // 
+            this.tmrSec.Interval = 1000;
+            this.tmrSec.Tick += new System.EventHandler(this.tmrSec_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 1);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.label1.Size = new System.Drawing.Size(149, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Pomodoro";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(327, 65);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btPlayPause);
+            this.Controls.Add(this.pgTime);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pomodoro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.ProgressBar pgTime;
+        private System.Windows.Forms.Button btPlayPause;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem timeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startStopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem workToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem breakToolStripMenuItem;
+        private System.Windows.Forms.Timer tmrSec;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem workToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem breakToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem alwaysVisibleToolStripMenuItem;
+    }
+}
+
---
+++ Pomodoro/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Pomodoro
+{
+    public partial class Form1 : Form
+    {
+        
+        bool IsBreak = false;
+
+        int _tmrSecs;
+        int TimerSecs
+        {
+            get
+            {
+                return _tmrSecs;
+            }
+
+            set
+            {
+                _tmrSecs = value;
+
+                if (IsBreak)
+                    lblTime.Text = GetTimeString(BreakSecs - value);
+                else
+                    lblTime.Text = GetTimeString(WorkSecs - value);
+            }
+        }
+
+        int _wrksecs;
+        int WorkSecs
+        {
+            get
+            {
+                return _wrksecs;
+            }
+
+            set
+            {
+                _wrksecs = value;
+
+                if (!IsBreak)
+                    lblTime.Text = GetTimeString(value);
+            }
+        }
+
+        int _brksecs;
+        int BreakSecs
+        {
+            get
+            {
+                return _brksecs;
+            }
+
+            set
+            {
+                _brksecs = value;
+                if (IsBreak)
+                    lblTime.Text = GetTimeString(value);
+            }
+        }
+
+        string GetTimeString(int secs)
+        {
+            if (secs > 3599)
+                return (secs / 3600) + ":" + ((secs - ((secs / 3600) * 3600)) / 60).ToString("00") + ":" + (secs - ((secs / 3600) * 3600) - (((secs - ((secs / 3600) * 3600)) / 60) * 60)).ToString("00");
+            if (secs > 59)
+                return (secs / 60).ToString("00") + ":" + (secs - ((secs / 60) * 60)).ToString("00");
+            else
+                return secs.ToString("00");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IsBreak = false;
+
+            WorkSecs = 1500;
+            BreakSecs = 300;
+
+            TimerSecs = 0;
+
+            TopMost = true;
+
+            menuStrip1.MouseDown += Form1_MouseDown;
+            label1.MouseDown+= Form1_MouseDown;
+
+            alwaysVisibleToolStripMenuItem.CheckedChanged += AlwaysVisibleToolStripMenuItem_CheckedChanged;
+        }
+
+        private void AlwaysVisibleToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = alwaysVisibleToolStripMenuItem.Checked;
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void workToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Value in minutes: ", "Work Time", (WorkSecs / 60).ToString());
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                int val = 0;
+                if (int.TryParse(input, out val))
+                    WorkSecs = 60 * val;
+            }
+        }
+
+        private void breakToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Value in minutes: ", "Break Time", (BreakSecs / 60).ToString());
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                int val = 0;
+                if (int.TryParse(input, out val))
+                    BreakSecs = 60 * val;
+            }
+        }
+
+        private void btPlayPause_Click(object sender, EventArgs e)
+        {
+            if (tmrSec.Enabled)
+                Pause();
+            else
+                Play();
+        }
+
+        void Play()
+        {
+            pgTime.Maximum = IsBreak ? BreakSecs : WorkSecs;
+            if (TimerSecs == 0)
+                pgTime.Value = 0;
+
+            workToolStripMenuItem.Enabled = false;
+            breakToolStripMenuItem.Enabled = false;
+
+            btPlayPause.BackgroundImage = Properties.Resources.pause;
+            btPlayPause.BackColor = Color.DeepPink;
+
+            tmrSec.Enabled = true;
+        }
+
+        void Pause()
+        {
+            tmrSec.Enabled = false;
+            btPlayPause.BackgroundImage = Properties.Resources.play;
+            btPlayPause.BackColor = Color.DarkSeaGreen;
+
+            workToolStripMenuItem.Enabled = true;
+            breakToolStripMenuItem.Enabled = true;         
+        }
+
+        private void startStopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tmrSec.Enabled)
+                Pause();
+            else
+                Play();
+        }
+
+        private void tmrSec_Tick(object sender, EventArgs e)
+        {
+            TimerSecs += 1;
+            pgTime.Value = TimerSecs;
+
+            if (IsBreak)
+            {
+                if (TimerSecs == BreakSecs)
+                {
+                    Pause();
+                    IsBreak = false;
+                    TimerSecs = 0;
+
+                    MessageBox.Show("Work time!", "Work time!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }                 
+            }
+            else
+            {
+                if (TimerSecs == WorkSecs)
+                {
+                    Pause();
+                    IsBreak = true;
+                    TimerSecs = 0;
+
+                    MessageBox.Show("Break time!", "Break time!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void workToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Pause();
+            IsBreak = false;
+            TimerSecs = 0;
+
+            pgTime.Value = 0;
+        }
+
+        private void breakToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Pause();
+            IsBreak = true;
+            TimerSecs = 0;           
+
+            pgTime.Value = 0;
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
